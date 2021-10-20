9e8648fdf0b2863a1d76aac33de06946b147fb36
positive-man <jwj0104@gmail.com>
2021-10-20 23:35:12 +0900

    Implement test for connecting with remote webdriver

--- finance-collector/src/test/kotlin/app/properstock/financecollector/ChromeAgentConnectTest.kt
+++ finance-collector/src/test/kotlin/app/properstock/financecollector/ChromeAgentConnectTest.kt
@@ -1,23 +1,25 @@
 package app.properstock.financecollector
 
-import org.junit.jupiter.api.Test
 import org.openqa.selenium.By
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.remote.RemoteWebDriver
 import java.net.URL
 
-class ChromeAgentConnectTest {
-    @Test
-    fun testConnect() {
-        val driver = RemoteWebDriver(
-            URL("http://127.0.0.1:4444"),
-            ChromeOptions()
-        )
-        driver.get("https://www.google.com")
-        val outerHtml = driver.findElement(By.tagName("html"))
-            .getAttribute("outerHTML")
 
-        println(outerHtml)
-        driver.quit()
-    }
+fun main() {
+    print("Connecting with driver...")
+    val driver = RemoteWebDriver(
+        URL("http://218.147.138.41:4444"),
+        ChromeOptions()
+    )
+    println("Driver connected.")
+
+    val testUrl = "https://www.google.com"
+    println("Crawling test page...")
+    driver.get(testUrl)
+    val outerHtml = driver.findElement(By.tagName("html"))
+        .getAttribute("outerHTML")
+
+    println(outerHtml)
+    driver.quit()
 }
