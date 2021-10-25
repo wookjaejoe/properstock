c45b3f46fd792c935cea33d001eded4fb79fd6c6
positive-man <jwj0104@gmail.com>
2021-10-26 03:20:31 +0900

    (WIP)

--- finance-collector-py/KrxCurrentPriceFetcher.py
+++ finance-collector-py/KrxCurrentPriceFetcher.py
@@ -1,6 +1,11 @@
 from pykrx import stock as krx
 from datetime import date, timedelta
 import pandas as pd
+import pika
+from datetime import datetime
+import json
+from typing import *
+from dataclasses import dataclass
 
 
 def date_str(d: date):
@@ -27,6 +32,13 @@ def nearest_business_date() -> date:
     return df.index[-1]
 
 
+@dataclass
+class KrxCurrentPrice:
+    code: str
+    price: float
+    updated: datetime
+
+
 class KrxCurrentPriceFetcher:
     """
     한국거래소를 통해 현재가를 구하여 반환. 현재가 외에도 시가, 등락율 등 데이터 포함하고 있음.
@@ -35,18 +47,41 @@ class KrxCurrentPriceFetcher:
     def __init__(self):
         self.ref_date_str = date_str(nearest_business_date())
 
-    def fetch(self) -> pd.DataFrame:
-        return krx.get_market_price_change_by_ticker(
+    def fetch(self) -> List[KrxCurrentPrice]:
+        prices_by_code = krx.get_market_price_change_by_ticker(
             fromdate=self.ref_date_str,
             todate=self.ref_date_str,
             market='ALL'
+        )['종가'].to_dict()
+        now = datetime.now()
+        return [KrxCurrentPrice(code, price, now) for code, price in prices_by_code]
+
+
+class KrxCurrentPricePublisher:
+    def __init__(self):
+        self.fetcher = KrxCurrentPriceFetcher()
+        self.rmq_conn = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost', port=5672)
         )
+
+    def start(self):
+        channel = self.rmq_conn.channel()
+        channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+        # 여기서 전 값이랑 비교
+        while True:
+            self.fetcher.fetch()
+            # todo: publish message
+            channel.basic_publish(exchange='logs', routing_key='', body="Not implemented yet")
+
+    def close(self):
+        self.rmq_conn.close()
 
 
 def main():
-    fetcher = KrxCurrentPriceFetcher()
-    while True:
-        print(fetcher.fetch())
+    publisher = KrxCurrentPricePublisher()
+    publisher.start()
+    publisher.close()
 
 
 if __name__ == '__main__':
