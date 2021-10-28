5b66dc450acb06508a8bf8a448da45a59408145a
wookjae_jo <wookjae.jo@ybrain.com>
2021-10-28 17:46:28 +0900

    Fix calculation interface

--- finance-collector/src/main/kotlin/app/properstock/financecollector/service/proper/ProperPriceCalculator.kt
+++ finance-collector/src/main/kotlin/app/properstock/financecollector/service/proper/ProperPriceCalculator.kt
@@ -6,33 +6,37 @@ import org.springframework.stereotype.Service
 class ProperPriceCalculator {
 }
 
+data class ProperPrice(
+    val formulaArguments: List<Argument>,
+    val value: Double
+) {
+    data class Argument(
+        val name: String,
+        val value: Double
+    )
+}
+
 interface ProperPriceFormula {
     val id: String
     val title: String
     val description: String
-    val arguments: List<Argument<Any>>
 
-    fun calculate(): Double
-
-    data class Argument<T>(
-        val name: String,
-        val value: T
-    )
+    fun calculate(): ProperPrice
 }
 
-class EpsMultipliedByPer(
-    val eps: Double,
-    val per: Double
+class EpsMultipliedByPER(
+    private val eps: Double,
+    private val per: Double
 ) : ProperPriceFormula {
-    override val id = "EPS_PER"
+    override val id = "EPSXPER"
     override val title = "EPS × PER"
-    override val description = ""
-    override val arguments: List<ProperPriceFormula.Argument<Any>> = listOf(
-        ProperPriceFormula.Argument("EPS", eps),
-        ProperPriceFormula.Argument("PER", per)
-    )
+    override val description = "순이익에 밸류에이션을 곱해서 기업의 주가를 계산하는 방식으로, 기업의 이익에 기초한 대중적으로 통용되는 가치평가 방법이다."
 
-    override fun calculate(): Double {
-        return eps * per
-    }
+    override fun calculate(): ProperPrice = ProperPrice(
+        formulaArguments = listOf(
+            ProperPrice.Argument("EPS", eps),
+            ProperPrice.Argument("PER", per)
+        ),
+        value = eps * per
+    )
 }
