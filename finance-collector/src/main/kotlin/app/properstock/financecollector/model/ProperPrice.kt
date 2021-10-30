9b3b0c935db7952fe884854b25faf1c2779581dd
positive-man <jwj0104@gmail.com>
2021-10-31 02:08:25 +0900

    Fix a error that indexed value duplicated

--- finance-collector/src/main/kotlin/app/properstock/financecollector/model/ProperPrice.kt
+++ finance-collector/src/main/kotlin/app/properstock/financecollector/model/ProperPrice.kt
@@ -1,14 +1,21 @@
 package app.properstock.financecollector.model
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.index.CompoundIndex
 import org.springframework.data.mongodb.core.index.CompoundIndexes
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
 @CompoundIndexes(
-    CompoundIndex(name = "ticker_formula", def = "{'tickerCode' : 1, 'formulaSymbol': 1}"),
+    CompoundIndex(
+        name = "ticker_formula",
+        def = "{'tickerCode' : 1, 'formulaSymbol': 1}",
+        unique = true
+    ),
 )
 data class ProperPrice(
+    @Id
+    val id: String? = null,
     val tickerCode: String,
     val formulaSymbol: String,
     var value: Double,
--- finance-collector/src/main/kotlin/app/properstock/financecollector/repository/ProperPriceRepository.kt
+++ finance-collector/src/main/kotlin/app/properstock/financecollector/repository/ProperPriceRepository.kt
@@ -3,6 +3,6 @@ package app.properstock.financecollector.repository
 import app.properstock.financecollector.model.ProperPrice
 import org.springframework.data.mongodb.repository.MongoRepository
 
-interface ProperPriceRepository : MongoRepository<ProperPrice, Long> {
+interface ProperPriceRepository : MongoRepository<ProperPrice, String> {
     fun findByTickerCodeAndFormulaSymbol(tickerCode: String, formulaSymbol: String): ProperPrice?
 }
