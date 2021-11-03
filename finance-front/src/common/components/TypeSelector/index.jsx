8ddd2d45db07132d3e14ec63414d0ed0f8d3e017
mugoon.park <mugun.park@gmail.com>
2021-11-03 23:12:54 +0900

    Formula 타입 선택 컴포넌트 수정

--- finance-front/src/common/components/TypeSelector/index.jsx
+++ finance-front/src/common/components/TypeSelector/index.jsx
@@ -3,10 +3,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import './TypeSelector.css';
+import ProperHttp from '../../https/ProperHttp';
 
 const TypeSelector = ({ onChange }) => {
   const [show, setShow] = useState(false);
   const selectorRef = useRef();
+  const [formulas, setFormulas] = useState([]);
+  const [selected, setSelected] = useState({});
+
+  useEffect(() => {
+    ProperHttp.searchFormulas().then((res) => {
+      setFormulas(res);
+      setSelected(res[0]);
+    });
+  }, []);
 
   // 컴포넌트 외부 클릭시 이벤트 처리
   useEffect(() => {
@@ -40,6 +50,7 @@ const TypeSelector = ({ onChange }) => {
 
   const handleChangeType = useCallback(
     (type) => {
+      setSelected(type);
       onChange(type);
       setShow(false);
     },
@@ -50,33 +61,22 @@ const TypeSelector = ({ onChange }) => {
     <div className="type__button__selector" ref={selectorRef}>
       <div className="card card__button" onClick={handleShow}>
         <div className="type__button">
-          <p className="type__button__title">EPS × PER</p>
-          <div className="type__button__desc">
-            순이익에 밸류에이션을 곱해서 기업의 주가를 계산하는 방식으로, 기업의 이익에 기초한
-            대중적으로 통용되는 가치평가 방법이다.
-          </div>
+          <p className="type__button__title">{selected?.title}</p>
+          <div className="type__button__desc">{selected?.shortDescription}</div>
           <FontAwesomeIcon className="type__button__icon" icon={faChevronCircleDown} />
         </div>
       </div>
       <div className={`type__button__selector__list ${show ? 'active' : ''}`}>
-        <div className="card card__button" onClick={() => handleChangeType('EPSROE')}>
-          <div className="type__button">
-            <p className="type__button__title">EPS × ROE</p>
-            <div className="type__button__desc">
-              주가지수는 경제 성장률 + 물가 상승률로서 EPSPER 공식의 추정 PER을 ROE를 통해 적정
-              PER로 산출하는 방법으로 슈퍼개미 김정환님이 제시하는 만능 공식이다.
+        {formulas.map((formula, idx) => {
+          return (
+            <div className="card card__button" onClick={() => handleChangeType(formula)} key={idx}>
+              <div className="type__button">
+                <p className="type__button__title">{formula.title}</p>
+                <div className="type__button__desc">{formula.shortDescription}</div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="card card__button" onClick={() => handleChangeType('EPSPER')}>
-          <div className="type__button">
-            <p className="type__button__title">EPS × PER</p>
-            <div className="type__button__desc">
-              순이익에 밸류에이션을 곱해서 기업의 주가를 계산하는 방식으로, 기업의 이익에 기초한
-              대중적으로 통용되는 가치평가 방법이다.
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
