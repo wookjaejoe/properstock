ab0a042b71c54065ac95db3ae836cadf68ebb8b4
mugoon.park <mugun.park@gmail.com>
2021-12-05 16:35:39 +0900

    공통 코드 제거

--- finance-front/src/modules/proper/ProperAllList/index.jsx
+++ finance-front/src/modules/proper/ProperAllList/index.jsx
@@ -5,6 +5,7 @@ import FilterContainer from '../../../common/components/FilterContainer';
 import FilterListItem from '../../../common/components/FilterListItem';
 import PageContents from '../../../common/components/PageContents';
 import PageTitle from '../../../common/components/PageTitle';
+import StockTable from '../../../common/components/StockTable/StockTable';
 import TypeSelector from '../../../common/components/TypeSelector';
 import GlobalStore from '../../../store/GlobalStore';
 import UIStore from './UIStore';
@@ -83,89 +84,11 @@ const ProperAllList = observer(() => {
         ></TypeSelector>
         <div className="card mt">
           <p className="card__title">조회 목록</p>
-          <div className="table__container">
-            <table className="table custom-table">
-              <thead className="sticky">
-                <tr>
-                  <th className="pc-only">종목 코드</th>
-                  <th>종목 명</th>
-                  <th className="pc-only">마켓</th>
-                  <th className="pc-only">업종</th>
-                  <th className="number-cell">현재 가격</th>
-                  <th className="number-cell">목표 주가</th>
-                  <th className="number-cell">적정 주가</th>
-                  <th className="number-cell pc-only">차액</th>
-                  <th className="number-cell">괴리율</th>
-                  <th className="number-cell">PER</th>
-                  <th className="number-cell">ROE</th>
-                  <th className="pc-only">참고 데이터</th>
-                </tr>
-              </thead>
-              <tbody>
-                {UIStore.properPriceList.map((price, idx) => {
-                  const ticker = GlobalStore.tickers[price.tickerCode];
-                  const margin = price.value - ticker.price;
-                  const marginRate = (margin / ticker.price) * 100;
-                  return (
-                    <tr key={idx}>
-                      <td className="pc-only">{price.tickerCode}</td>
-                      <td onClick={() => goDetail(price.tickerCode)} className="go-detail">
-                        {price.tickerName}
-                      </td>
-                      <td className="pc-only">
-                        <span className={`badge ${ticker.market.toLowerCase()}`}>
-                          {ticker.market}
-                        </span>
-                      </td>
-                      <td className="pc-only">
-                        <span>{ticker.industry}</span>
-                      </td>
-                      <td className="number-cell">
-                        <span>{ticker.price.toLocaleString()}</span>
-                      </td>
-                      <td className="number-cell">
-                        <span>{ticker.targetPrice?.toLocaleString()}</span>
-                      </td>
-                      <td className="number-cell">
-                        <span>{parseInt(price.value).toLocaleString()}</span>
-                      </td>
-                      <td className="number-cell pc-only">
-                        <span className={margin > 0 ? 'font-green' : 'font-red'}>
-                          {parseInt(margin).toLocaleString()}
-                        </span>
-                      </td>
-                      <td className="number-cell">
-                        <span className={marginRate > 0 ? 'font-green' : 'font-red'}>
-                          {parseInt(marginRate)}%
-                        </span>
-                      </td>
-                      <td className="number-cell">
-                        <span>{ticker.per}</span>
-                      </td>
-                      <td className="number-cell">
-                        <span>{!Number.isNaN(Number.parseInt(ticker.roe)) && ticker.roe}</span>
-                      </td>
-                      <td className="pc-only">
-                        {ticker.externalLinks.map((link, index) => {
-                          return (
-                            <a
-                              className="external_link"
-                              href={link.url}
-                              key={index}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <span className="badge">{link.domainName}</span>
-                            </a>
-                          );
-                        })}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
+          <StockTable
+            properPriceList={UIStore.properPriceList}
+            onClick={(code) => UIStore.goDetails(code)}
+          />
+
           {/* <div className="pagination">
             <span>
               <FontAwesomeIcon icon={faAngleDoubleLeft} />
--- finance-front/src/modules/proper/ProperTop100/index.jsx
+++ finance-front/src/modules/proper/ProperTop100/index.jsx
@@ -3,6 +3,7 @@ import React, { useCallback } from 'react';
 import { useHistory } from 'react-router';
 import PageContents from '../../../common/components/PageContents';
 import PageTitle from '../../../common/components/PageTitle';
+import StockTable from '../../../common/components/StockTable/StockTable';
 import TypeSelector from '../../../common/components/TypeSelector';
 import GlobalStore from '../../../store/GlobalStore';
 import UIStore from './UIStore';
@@ -54,89 +55,10 @@ const ProperTop100 = observer(() => {
           formulas={GlobalStore.formulas}
           onChange={(type) => UIStore.changeType(type)}
         ></TypeSelector>
-        <div className="table__container">
-          <table className="table custom-table">
-            <thead className="sticky">
-              <tr>
-                <th className="pc-only">종목 코드</th>
-                <th>종목 명</th>
-                <th className="pc-only">마켓</th>
-                <th className="pc-only">업종</th>
-                <th className="number-cell">현재 가격</th>
-                <th className="number-cell">목표 주가</th>
-                <th className="number-cell">적정 주가</th>
-                <th className="number-cell pc-only">차액</th>
-                <th className="number-cell">괴리율</th>
-                <th className="number-cell">PER</th>
-                <th className="number-cell">ROE</th>
-                <th className="pc-only">참고 데이터</th>
-              </tr>
-            </thead>
-            <tbody>
-              {UIStore.properPriceTop100.map((price, idx) => {
-                const ticker = GlobalStore.tickers[price.tickerCode];
-                const margin = price.value - ticker.price;
-                const marginRate = (margin / ticker.price) * 100;
-                return (
-                  <tr key={idx}>
-                    <td className="pc-only">{price.tickerCode}</td>
-                    <td onClick={() => goDetails(price.tickerCode)} className="go-detail">
-                      {price.tickerName}
-                    </td>
-                    <td className="pc-only">
-                      <span className={`badge ${price.tickerMarket.toLowerCase()}`}>
-                        {price.tickerMarket}
-                      </span>
-                    </td>
-                    <td className="pc-only">
-                      <span>{price.tickerIndustry}</span>
-                    </td>
-                    <td className="number-cell">
-                      <span>{ticker.price.toLocaleString()}</span>
-                    </td>
-                    <td className="number-cell">
-                      <span>{ticker.targetPrice?.toLocaleString()}</span>
-                    </td>
-                    <td className="number-cell">
-                      <span>{parseInt(price.value).toLocaleString()}</span>
-                    </td>
-                    <td className="number-cell pc-only">
-                      <span className={margin > 0 ? 'font-green' : 'font-red'}>
-                        {parseInt(margin).toLocaleString()}
-                      </span>
-                    </td>
-                    <td className="number-cell">
-                      <span className={marginRate > 0 ? 'font-green' : 'font-red'}>
-                        {parseInt(marginRate)}%
-                      </span>
-                    </td>
-                    <td className="number-cell">
-                      <span>{ticker.per}</span>
-                    </td>
-                    <td className="number-cell">
-                      <span>{!Number.isNaN(Number.parseInt(ticker.roe)) && ticker.roe}</span>
-                    </td>
-                    <td className="pc-only">
-                      {ticker.externalLinks.map((link, index) => {
-                        return (
-                          <a
-                            className="external_link"
-                            href={link.url}
-                            key={index}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            <span className="badge">{link.domainName}</span>
-                          </a>
-                        );
-                      })}
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+        <StockTable
+          properPriceList={UIStore.properPriceTop100}
+          onClick={(code) => UIStore.goDetails(code)}
+        />
       </PageContents>
     </>
   );
